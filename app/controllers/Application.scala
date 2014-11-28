dba65f5aee68e816ed7ac0997aebc5f08f075619
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-28 14:51:19 +0100

    HTTP API for munin \o/

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,10 @@
 package controllers
 
 import play.api._
-import play.api.mvc._
 import play.api.libs._
-import play.api.libs.json._
 import play.api.libs.iteratee._
+import play.api.libs.json._
+import play.api.mvc._
 
 object Application extends Controller {
 
@@ -14,25 +14,32 @@ object Application extends Controller {
 
   val (events, channel) = Concurrent.broadcast[JsValue]
 
+  // YOLO
+  val lastSeen = collection.mutable.Map.empty[String, Any]
+
+  def push[A: Writes](key: String, value: A) {
+    channel.push(Json.obj(key -> value))
+    lastSeen += (key -> value.toString)
+  }
+
   def light(state: Int) = Action {
-    channel.push(Json.obj("light" -> (state == 1)))
+    push("light", state == 1)
     Ok
   }
 
   def humidity(value: Int) = Action {
     // Assuming that value range is from 0 -> 100
-    val percentage = value.toDouble / 100.0
-    channel.push(Json.obj("humidity" -> percentage))
+    push("humidity", value.toDouble / 100.0)
     Ok
   }
 
   def temperature(value: Int) = Action {
-    channel.push(Json.obj("temperature" -> value))
+    push("temperature", value)
     Ok
   }
 
   def sound(value: Int) = Action {
-    channel.push(Json.obj("sound" -> value))
+    push("sound", value)
     Ok
   }
 
@@ -42,4 +49,12 @@ object Application extends Controller {
     ) as "text/event-stream"
   }
 
+  def get(key: String) = Action {
+    lastSeen get key match {
+      case Some("true")  => Ok("1")
+      case Some("false") => Ok("0")
+      case Some(v)     => Ok(v.toString)
+      case None        => NotFound
+    }
+  }
 }
